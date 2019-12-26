28ecfb74eb063a903a6330d94b5f68bbb385d540
Dragos Zavate <zavatedragos1984@gmail.com>
2019-12-26 08:41:28 +0000

    new install script

--- cctv-app/stage/update_install.sh
+++ cctv-app/stage/update_install.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 yum update -y
 yum install git -y
-amazon-linux-extras install docker
+amazon-linux-extras install docker -y
 service docker start
-git clone https://github.com/silverescu/open-docker.git s3fs-fuse
+mkdir s3fs-fuse
 cd s3fs-fuse
 aws s3 cp s3://drazvt-config-files/env.list .
-docker build --rm -t drzvt/s3fs-fuse-cctv:1.0 .
+docker pull drzvt/s3fs-fuse
 docker run --restart always -p 21:21 -p 30000-30100:30000-30100 --privileged --env-file env.list drzvt/s3fs-fuse-cctv:1.0
